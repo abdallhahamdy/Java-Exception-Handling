8153fcdda8c76b10eed6250a27164241156934db
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-11-27 22:17:11 +0200

    java.lang.Throwable Class in Java

---
+++ java.lang.Throwable Class in Java/ThrowableExample.java
@@ -0,0 +1,17 @@
+public class ThrowableExample {
+    public static void main(String[] args) {
+        try{
+            throwException(null);
+        }catch (Throwable e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getClass());
+        }
+    }
+
+    private static void throwException(String str) throws Throwable{
+        if(str == null){
+            throw new Throwable("String is null.");
+        }
+    }
+}
