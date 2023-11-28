6cb245991dbca9448023d7e9cc884853ef443e4a
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-11-28 23:56:08 +0200

    Java trycatch Block

---
+++ 7- Java trycatch Block/ArrayIndexOutOfBounds.java
@@ -0,0 +1,18 @@
+public class ArrayIndexOutOfBounds {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[] { 1, 2, 3 };
+
+        try {
+            int numFromNegativeIndex = nums[-1]; // Trying to access at negative index
+            int numFromGreaterIndex = nums[4]; // Trying to access at greater index
+            int numFromLengthIndex = nums[3]; // Trying to access at index equal to size of the array
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException caught");
+        }
+
+        System.out.println("rest of the code executes");
+    }
+
+}
